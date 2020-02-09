d4c4af72b974e45fe61f94e0aefee09ba2280b0e
Taylor Cannon <tcannon@brighthealthplan.com>
2020-02-09 09:27:28 -0600

    testing commit

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('issues.opened', async (context) => {
+    console.log('Trying to leave a comment');
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
+    console.log('Comment Left');
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
