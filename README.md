30c4f4a32f1ff18950f1186eaab81a90a5467fc4
Taylor Cannon <tcannon@brighthealthplan.com>
2020-02-11 09:27:30 -0600

    [GH-9]feat: Add PR Title checking (#8)
    * [GH-11]feat: Add Conventional Commit Checking
    
    - Update the permissions to allow for read/write of PRs, access to code, and remove access to issues
    - Add dummy data for testing pull request actions
    	- Add testing scripts that leverage that dummy data
    - Add code to check for conventional commits on any changes to the pull request
    
    * [GH-9] Update README and regex
    
    - README now has docs on the rules being enforced
    - regex for the page no longer follows conventional commits

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 > A GitHub App built with [Probot](https://github.com/probot/probot) that Enforces requirements for bheng PRs
 
+## Bright Health PR Rules
+### PR Title
+The commits for a PR are squashed when merged to master for all repos in the Consumer Platformer team. When Github does this, it take the PR's title and that becomes the primary commit on master with the extended description being the individual commits from the PR. To keep master's history parseable, easy to identify changes to revert (if necessary), and for compliance/auditing purposes, all commits on master should contain the Jira ticket for which the work was done. This rule enforces that the PR title be in the following format:
+```
+[<Jira Ticket ID>] <PR Description>
+```
+
+Example:
+```
+[CP-246] Adjusted header for IFP Plans page
+```
+
 ## Deploy updates
 To deploy the app to zeit:
 - Make sure to have the zeit cli installed
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
-export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    console.log('Trying to leave a comment');
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-    console.log('Comment Left');
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+const prTitleRegex = new RegExp(`^\\[(([A-Z]+-\d+)|(NO_TICKET))\\].+$`);
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+export = (app: Application) => {
+  app.on('pull_request', async (context) => {
+    let descriptionError = false;
+    let squashedError = false;
+    let signOffsError = false;
+
+    const githubDetails = {
+      owner: context.payload.repository.owner.login,
+      pull_number: context.payload.pull_request.number,
+      repo: context.payload.repository.name,
+    };
+
+    const titleToParse = context.payload.pull_request.title;
+    if (!prTitleRegex.test(titleToParse)) {
+      // DOES NOT MEET STANDARDS
+      await context.github.pulls.createReview({
+        ...githubDetails,
+        event: 'REQUEST_CHANGES',
+        body: `This PR does not meet our description standards, please update the title to the following format:
+          \`\`\`[<jira_id>] <PR Description>\`\`\`
+        `,
+      });
+
+      descriptionError = true;
+    }
+
+    // Everything is looking good
+    if (!descriptionError && !squashedError && !signOffsError) {
+      await context.github.pulls.createReview({
+        ...githubDetails,
+        event: 'APPROVE',
+        body: "All checks passing, you are good to go.",
+      });
+    }
+  });
 }
